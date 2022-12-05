c57fa89dbf1f2cfb496b941689569169238aa692
n0spoon <xtrifo@gmail.com>
2022-12-05 18:10:49 +0200

    Siistitty luokkia pylintin mukaisiksi ja tehty lisää pylint konfiguraatiota

--- src/refclasses/bookref.py
+++ src/refclasses/bookref.py
@@ -1,5 +1,5 @@
 class Bookref:
-    def __init__(self, key: str, author: str, title: str, year: int, publisher: str ):
+    def __init__(self, key: str, author: str, title: str, year: int, publisher: str):
         self.type = "book"
         self.key = key
         self.author = author
@@ -26,4 +26,4 @@ class Bookref:
         return f"\033[0;33mkey\033[0m: \033[1;31m{self.key}\033[0m, \033[0;33mauthor\033[0m: \033[1;32m{self.author}\033[0m, \033[0;33mtitle\033[0m: \033[1;34m{self.title}\033[0m, \033[0;33myear\033[0m: \033[1;35m{str(self.year)}\033[0m, \033[0;33mpublisher\033[0m: \033[1;36m{self.publisher}\033[0m"
 
     def __lt__(self, other):
-         return self.author < other.author
+        return self.author < other.author
