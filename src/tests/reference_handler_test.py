687c895db6a8d89b994f52c454f44f6638787ae3
qusba <kasper.kahkonen@outlook.com>
2022-12-12 14:59:57 +0200

    Tein testejä uusille järjestämismetodeille; uusin ensin, aakkosjärjestys, käänteinen aakkosjärjestys

--- src/tests/reference_handler_test.py
+++ src/tests/reference_handler_test.py
@@ -34,10 +34,33 @@ class Test_ReferenceHandler(unittest.TestCase):
         self.mock_referencehandler.print_references(self.mock_filehandler.get_references())
         self.mock_io.write.assert_called()
 
-    def test_print_references_outputs_as_expected(self):
+    def test_print_references_in_oldest_first_order_outputs_as_expected(self):
         self.mock_referencehandler.generate_book_reference_object(self.inputs)
         self.mock_referencehandler.print_references(self.mock_filehandler.get_references())
         printed = self.mock_io.get_log()[0]
         self.assertEqual(printed,"\033[0;33mkey\033[0m: \033[1;31mTestiavain\033[0m, \033[0;33mauthor\033[0m: \033[1;32mkirjailija_etu\033[0m, \033[1;32mkirjailija_suku\033[0m, \033[0;33mtitle\033[0m: \033[1;34mTestititle\033[0m, \033[0;33myear\033[0m: \033[1;35m1999\033[0m, \033[0;33mpublisher\033[0m: \033[1;36mTestikustantaja\033[0m")
 
+    def test_print_references_in_newest_first_order_outputs_as_expected(self):
+        secondary_inputs = ["Toinenavain","Antti","Arvuuttaja","Toinentitle",2000,"Otava"]
+        self.mock_referencehandler.generate_book_reference_object(self.inputs)
+        self.mock_referencehandler.generate_book_reference_object(secondary_inputs)
+        self.mock_referencehandler.print_references(self.mock_referencehandler.reverse_the_reference_list())
+        printed = self.mock_io.get_log()[0]
+        self.assertEqual(printed, "\033[0;33mkey\033[0m: \033[1;31mToinenavain\033[0m, \033[0;33mauthor\033[0m: \033[1;32mAntti\033[0m, \033[1;32mArvuuttaja\033[0m, \033[0;33mtitle\033[0m: \033[1;34mToinentitle\033[0m, \033[0;33myear\033[0m: \033[1;35m2000\033[0m, \033[0;33mpublisher\033[0m: \033[1;36mOtava\033[0m")
 
+
+    def test_print_references_in_alphabetical_order_outputs_as_expected(self):
+        secondary_inputs = ["Toinenavain","Antti","Arvuuttaja","Toinentitle",2000,"Otava"]
+        self.mock_referencehandler.generate_book_reference_object(self.inputs)
+        self.mock_referencehandler.generate_book_reference_object(secondary_inputs)
+        self.mock_referencehandler.print_references(self.mock_referencehandler.sort_refs_to_alphabetical_order_by_author_surname())
+        printed = self.mock_io.get_log()[0]
+        self.assertEqual(printed, "\033[0;33mkey\033[0m: \033[1;31mToinenavain\033[0m, \033[0;33mauthor\033[0m: \033[1;32mAntti\033[0m, \033[1;32mArvuuttaja\033[0m, \033[0;33mtitle\033[0m: \033[1;34mToinentitle\033[0m, \033[0;33myear\033[0m: \033[1;35m2000\033[0m, \033[0;33mpublisher\033[0m: \033[1;36mOtava\033[0m")
+
+    def test_print_refereces_in_reversed_alphabetical_order_outputs_as_expected(self):
+        secondary_inputs = ["Toinenavain","Antti","Arvuuttaja","Toinentitle",2000,"Otava"]
+        self.mock_referencehandler.generate_book_reference_object(self.inputs)
+        self.mock_referencehandler.generate_book_reference_object(secondary_inputs)
+        self.mock_referencehandler.print_references(self.mock_referencehandler.sort_refs_to_reverse_alphabetical_order_by_author_surname())
+        printed = self.mock_io.get_log()[0]
+        self.assertEqual(printed,"\033[0;33mkey\033[0m: \033[1;31mTestiavain\033[0m, \033[0;33mauthor\033[0m: \033[1;32mkirjailija_etu\033[0m, \033[1;32mkirjailija_suku\033[0m, \033[0;33mtitle\033[0m: \033[1;34mTestititle\033[0m, \033[0;33myear\033[0m: \033[1;35m1999\033[0m, \033[0;33mpublisher\033[0m: \033[1;36mTestikustantaja\033[0m")
