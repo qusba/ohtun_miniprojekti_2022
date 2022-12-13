39660690cb9f84bd0d0ffd81b862d73bf47c8752
Niina-Maria Takalo <niina-maria.takalo@helsinki.fi>
2022-12-13 19:55:06 +0200

    tein yksikkötestin tag_setterille

--- src/tests/bookref_test.py
+++ src/tests/bookref_test.py
@@ -5,6 +5,9 @@ class Test_Bookref(unittest.TestCase):
 
     def setUp(self):
         self.bookref = Bookref("Testiavain","Testikirjailija_etunimi", "Testikirjailija_sukunimi", "Testititle", 1999, "Testikustantaja")
+        self.tags = []
+        self.tags.append("Fiktio")
+        self.tags.append("Fakta")
 
     def test_getters_behave_as_intended(self):
         self.assertEqual(self.bookref.get_key(),"Testiavain")
@@ -18,3 +21,7 @@ class Test_Bookref(unittest.TestCase):
         printti = str(self.bookref)
         #self.assertEqual(printti,"key: Testiavain, author: Testinimi, title: Testititle, year: 1999, publisher: Testikustantaja")
         self.assertEqual(printti, "\033[0;33mkey\033[0m: \033[1;31mTestiavain\033[0m, \033[0;33mauthor\033[0m: \033[1;32mTestikirjailija_etunimi\033[0m, \033[1;32mTestikirjailija_sukunimi\033[0m, \033[0;33mtitle\033[0m: \033[1;34mTestititle\033[0m, \033[0;33myear\033[0m: \033[1;35m1999\033[0m, \033[0;33mpublisher\033[0m: \033[1;36mTestikustantaja\033[0m")
+
+    def test_tag_setter_sets_tags(self):
+        self.bookref.set_tags(self.tags)
+        self.assertEqual(self.bookref.get_tags()[0], "Fiktio")
